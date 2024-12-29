08a56e748ca28f6631f0a0df7178f17ded014d19
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2024-12-29 10:39:15 +1000

    Update event

--- src/pocketmine/event/player/PlayerInteractEntityEvent.php
+++ src/pocketmine/event/player/PlayerInteractEntityEvent.php
@@ -20,6 +20,7 @@
  *
  */
 
+
 declare(strict_types=1);
 
 namespace pocketmine\event\player;
@@ -28,51 +29,29 @@ use pocketmine\entity\Entity;
 use pocketmine\event\Cancellable;
 use pocketmine\item\Item;
 use pocketmine\math\Vector3;
-use pocketmine\Player;
+use pocketmine\player\Player;
 
 /**
- * Called when a player interacts with an entity
+ * Called when a player interacts with an entity.
  */
-class PlayerInteractEntityEvent extends PlayerEvent implements Cancellable{
+class PlayerInteractEntityEvent extends PlayerEvent implements Cancellable {
 
-	/** @var Entity */
-	protected $entity;
-	/** @var Item */
-	protected $item;
-	/** @var Vector3 */
-	protected $clickPos;
+    public function __construct(
+        public readonly Player $player,
+        public readonly Entity $entity,
+        public readonly Item $item,
+        public readonly Vector3 $clickPos
+    ) {}
 
-	/**
-	 * @param Player  $player
-	 * @param Entity  $entity
-	 * @param Item    $item
-	 * @param Vector3 $clickPos
-	 */
-	public function __construct(Player $player, Entity $entity, Item $item, Vector3 $clickPos){
-		$this->player = $player;
-		$this->entity = $entity;
-		$this->item = $item;
-		$this->clickPos = $clickPos;
-	}
+    public function getEntity(): Entity {
+        return $this->entity;
+    }
 
-	/**
-	 * @return Entity
-	 */
-	public function getEntity() : Entity{
-		return $this->entity;
-	}
+    public function getItem(): Item {
+        return $this->item;
+    }
 
-	/**
-	 * @return Item
-	 */
-	public function getItem() : Item{
-		return $this->item;
-	}
-
-	/**
-	 * @return Vector3
-	 */
-	public function getClickPosition() : Vector3{
-		return $this->clickPos;
-	}
+    public function getClickPosition(): Vector3 {
+        return $this->clickPos;
+    }
 }
